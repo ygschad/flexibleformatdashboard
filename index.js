bd4cdf3008309bb6bc59b14fcfd95e2808ec0d05
ygschad <gs1ann.hens@hotmail.com>
2024-05-07 10:25:06 +0700

    hi commit

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-const countOccurrences = (arr, value) =>
-  arr.reduce((a, v) => (v === value ? a + 1 : a), 0);
+function isValidSudoku(board) {
+  const rows = new Array(9).fill().map(() => new Array(9).fill(0));
+  const cols = new Array(9).fill().map(() => new Array(9).fill(0));
+  const boxes = new Array(9).fill().map(() => new Array(9).fill(0));
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (board[i][j] !== ".") {
+        const num = Number(board[i][j]) - 1;
+        const k = Math.floor(i / 3) * 3 + Math.floor(j / 3);
+        if (rows[i][num] || cols[j][num] || boxes[k][num]) return false;
+        rows[i][num] = 1;
+        cols[j][num] = 1;
+        boxes[k][num] = 1;
+      }
+    }
+  }
+  return true;
+}
