2e10c76178ec77cb69c6153250de71108e0e399b
ygschad <gs1ann.hens@hotmail.com>
2024-05-10 06:12:10 +0700

    commit

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-function canJump(nums) {
-  let lastPos = nums.length - 1;
-  for (let i = nums.length - 2; i >= 0; i--) {
-    if (i + nums[i] >= lastPos) lastPos = i;
+function mergeTwoLists(l1, l2) {
+  if (!l1) return l2;
+  if (!l2) return l1;
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
   }
-  return lastPos === 0;
 }
